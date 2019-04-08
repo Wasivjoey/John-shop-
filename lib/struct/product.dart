8cc1a381159174e2e0e7b84a277aa8cb4875029d
Phillip Cole <42485975+philquake@users.noreply.github.com>
2019-04-08 18:57:20 -0500

    Adjusted product structure

--- lib/struct/product.dart
+++ lib/struct/product.dart
@@ -1,28 +1,22 @@
 class Product {
-  String _id;
   String _productName;
   String _productPrice;
   String _productPicture;
 
-  Product(this._id, this._productName, this._productPrice, this._productPicture);
+  Product(this._productName, this._productPrice, this._productPicture);
 
   Product.map(dynamic obj) {
-    this._id = obj['id'];
     this._productName = obj['name'];
     this._productPrice = obj['price'];
     this._productPicture = obj['picture'];
   }
 
-  String get id => _id;
   String get productName => _productName;
   String get productPrice => _productPrice;
   String get productPicture => _productPicture;
 
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
-    if (_id != null) {
-      map['id'] = _id;
-    }
     map['name'] = _productName;
     map['price'] = _productPrice;
     map['picture'] = _productPicture;
@@ -31,7 +25,6 @@ class Product {
   }
 
   Product.fromMap(Map<String, dynamic> map) {
-    this._id = map['id'];
     this._productName = map['name'];
     this._productPrice = map['price'];
     this._productPicture = map['picture'];
