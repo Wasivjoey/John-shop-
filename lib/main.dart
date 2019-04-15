f820dab390dde62ac778d7f8ed5267949bfe69ea
Phillip Cole <42485975+philquake@users.noreply.github.com>
2019-04-15 10:00:41 -0500

    Adjustment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_pro/carousel_pro.dart';
-import 'package:john_shop_mob/pages/horizontal_list_view.dart';
-import 'package:john_shop_mob/pages/product_page.dart';
-import 'package:john_shop_mob/pages/cart_page.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
-import 'home.dart';
+import 'pages/home.dart';
 
 void main() => runApp(MyApp());
 
